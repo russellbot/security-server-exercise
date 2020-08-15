dc17c30618e8761d0d2c329c47cec73d97baf8a8
Russell Hart <russellbot@gmail.com>
2020-08-15 17:42:28 -0400

    added content-security-policy and secure httpOnly cookies

--- server.js
+++ server.js
@@ -12,7 +12,14 @@ app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.cookie('session', '1', { httpOnly: true })
+  res.cookie('session', '1', { secure: true })
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https.//apis.google.com'"
+  })
+  res.send('Hello World!')
+});
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
